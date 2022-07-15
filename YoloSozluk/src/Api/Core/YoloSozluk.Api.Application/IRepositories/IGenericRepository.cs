a08ce8a245b3fab07fb930bc2d56f3bcb596f466
Berkay Küçük <berkay.kucuk@ogr.sakarya.edu.tr>
2022-07-15 22:02:21 +0300

    Added IGenericRepository Interface

---
+++ YoloSozluk/src/Api/Core/YoloSozluk.Api.Application/IRepositories/IGenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using YoloSozluk.Api.Domain.Entities.Base;
+
+namespace YoloSozluk.Api.Application.IRepositories
+{
+    public interface IGenericRepository<TEntity>  where TEntity : BaseModel
+    {
+
+        Task<int> AddAsync(TEntity entity);
+        Task<int> AddAsync(IEnumerable<TEntity> entity);
+        int Add(TEntity entity);
+        int Add(IEnumerable<TEntity> entity);
+
+
+        Task<int> UpdateAsync(IEnumerable<TEntity> entity);
+        int Update(TEntity entity);
+
+        Task<int> DeleteAsync(Guid id );
+        int Delete(Guid id);
+        Task<int>DeleteAsync(IEnumerable<TEntity> entity);
+        int Delete(TEntity entity);
+        Task<int> DeleteRangeAsync(Expression<Func<TEntity,bool>> predicate);
+        bool DeleteRange(Expression<Func<TEntity, bool>> predicate);
+
+
+        Task<int> AddOrUpdateAsync(TEntity entity);
+        int AddOrUpdate(TEntity entity);
+
+        IQueryable<TEntity> AsQueryAble();
+        Task<List<TEntity>> GetAllAsync(bool noTracking = true);
+
+
+        Task<List<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> predicate, bool noTracking = true,Func<IQueryable<TEntity>,IOrderedQueryable<TEntity>>  orderBy = null, params                                                          Expression<Func<TEntity,object>>[] includes);
+
+        Task<TEntity> GetByIdAsync(Guid id, bool noTracking = true, params Expression<Func<TEntity, object>>[] includes);
+        Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> predicate, bool noTracking = true, params Expression<Func<TEntity, object>>[] includes);
+        Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, bool noTracking = true, params Expression<Func<TEntity, object>>[] includes);
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate, bool noTracking = true, params Expression<Func<TEntity, object>>[] includes);
+
+        Task BulkDeleteById(IEnumerable<Guid> ids);
+        Task BulkDelete(Expression<Func<TEntity, bool>> predicate);
+        Task BulkDelete(IEnumerable<TEntity> entities);
+        Task BulkUpdate(IEnumerable<TEntity> entities);
+        Task BulkAdd(IEnumerable<TEntity> entities);
+    }
+}
