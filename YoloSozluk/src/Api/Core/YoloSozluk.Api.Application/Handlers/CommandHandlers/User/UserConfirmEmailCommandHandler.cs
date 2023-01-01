406535a098068076d50415d8e1a276d97ba98a23
Berkay Küçük <berkay.kucuk@ogr.sakarya.edu.tr>
2023-01-01 23:05:42 +0300

    Created User Email Confirmation

---
+++ YoloSozluk/src/Api/Core/YoloSozluk.Api.Application/Handlers/CommandHandlers/User/UserConfirmEmailCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YoloSozluk.Api.Application.IRepositories;
+using YoloSozluk.Common.Exceptions.User;
+using YoloSozluk.Common.Models.Commands;
+
+namespace YoloSozluk.Api.Application.Handlers.CommandHandlers.User
+{
+    public class UserConfirmEmailCommandHandler : IRequestHandler<UserConfirmEmailCommand, bool>
+    {
+        private readonly IUserRepository _userRepo;
+        private readonly IEmailConfirmationRepository _confRepo;
+
+        public UserConfirmEmailCommandHandler(IUserRepository userRepo, IEmailConfirmationRepository confRepo)
+        {
+            _userRepo = userRepo;
+            _confRepo = confRepo;
+        }
+
+        public async Task<bool> Handle(UserConfirmEmailCommand request, CancellationToken cancellationToken)
+        {
+            var confirmation = await _confRepo.GetByIdAsync(request.ConfirmationId);
+
+            if (confirmation is null)
+                throw new UserMailConfirmationException("Confirmation not found!");
+
+            var user = await _userRepo.GetSingleAsync(x => x.Email == confirmation.NewEmailAdress);
+
+            if (user is null)
+                throw new UserException("User not found!");
+
+            if (user.EmailConfirmed)
+                throw new UserMailConfirmationException("Email address is already confirmed!");
+
+            user.EmailConfirmed = true;
+
+            await _userRepo.UpdateAsync(user);
+            return true;
+
+        }
+    }
+}
---
+++ YoloSozluk/src/Common/YoloSozluk.Common/Exceptions/User/UserMailConfirmationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoloSozluk.Api.Application.Dtos;
+using YoloSozluk.Common.Exceptions.Base;
+
+namespace YoloSozluk.Common.Exceptions.User
+{
+    public class UserMailConfirmationException:ExceptionBase
+    {
+		public UserMailConfirmationException(string message) : base(message)
+		{
+		}
+
+		public UserMailConfirmationException(IEnumerable<ErrorBaseDto> errors, string message) : base(errors, message)
+		{
+		}
+	}
+}
